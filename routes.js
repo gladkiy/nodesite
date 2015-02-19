3e25093dfd43f6856d5588b7e8bf2f7b97038533
Alexander Gladkiy <aa_glad@mail.ru>
2015-02-19 20:37:43 +0300

    Added simple pages server.

--- routes.js
+++ routes.js
@@ -2,6 +2,12 @@
 
 var app = require('./app');
 
-app.get('/', function(req, res){
-	res.send('Hello world!');
+app.get('/', function(req, res, next){
+	req.url = '/index.html';
+	//res.render('pages/index');
+	next();
+});
+
+app.get('/*.html', function(req, res, next){
+	res.render('pages/' + req.params[0]);
 });
